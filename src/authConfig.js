36d5085c5c62acfc27c884867d1cefe58dab1017
DomGibson <djoegibson01@gmail.com>
2024-09-09 15:31:27 +0100

    Updated files

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,11 +1,11 @@
 // src/authConfig.js
 
 export const msalConfig = {
-    auth: {
-      clientId: process.env.CLIENT_ID, // Replace with your app's Client ID
-      authority: process.env.AUTHORITY_URL, // Replace with your Tenant ID
-      redirectUri: process.env.REDIRECT_URL, // The redirect URI you set in the Azure portal
-    },
+  auth: {
+    clientId: process.env.REACT_APP_CLIENT_ID, // Replace with your app's Client ID
+    authority: process.env.REACT_APP_AUTHORITY_URL, // Replace with your Tenant ID
+    redirectUri: process.env.REACT_APP_REDIRECT_URL, // The redirect URI you set in the Azure portal
+  },
     cache: {
       cacheLocation: "sessionStorage", // This configures where your cache will be stored
       storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
